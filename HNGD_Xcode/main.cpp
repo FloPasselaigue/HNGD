bacf823c062fa1ab1521ec03df1c668766621a39
passflor <fpp8@psu.edu>
2020-05-11 08:38:40 -0400

    Initial Commit

---
+++ HNGD_Xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HNGD_Xcode
+//
+//  Created by Passelaigue, Florian on 5/11/20.
+//  Copyright © 2020 Passelaigue, Florian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
